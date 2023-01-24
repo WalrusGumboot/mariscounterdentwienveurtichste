3c199fd2d1b5706790792cc7bd278d31200c11c2
Simeon Duwel <tameimpalafan123@gmail.com>
2023-01-24 18:32:35 +0100

    added two more counters

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,39 @@
 <script lang="ts">
-let tellers = ["hè", "klaar", "oké"].map(e => { return {woord: e, aantal: 0} });
+ interface Teller {
+   woord: string,
+   aantal: number,
+   toets: string
+ }
 
-function meer_graag(index: number) {
-  tellers[index].aantal = tellers[index].aantal + 1;
-  tellers = tellers;
-}
+ function maakTeller(woord: string, toets: string): Teller {
+   return {
+     woord: woord,
+     toets: toets,
+     aantal: 0
+   }
+ }
 
+ let tellers = [
+   maakTeller("hè", "KeyQ"),
+   maakTeller("klaar", "KeyW"),
+   maakTeller("oké", "KeyE"),
+   maakTeller("ja", "Space"),
+   maakTeller("is goed", "KeyR"),
+ ];
+
+
+ function incr(i) {
+   tellers[i].aantal = tellers[i].aantal + 1;
+   tellers = tellers;
+ }
 function keypress(e) {
-  switch (e.code) {
-      case "KeyQ": meer_graag(0); break;
-      case "KeyW": meer_graag(1); break;
-      case "KeyE": meer_graag(2); break;
-      default: break;
-  }
+   for (let t of tellers) {
+     if (e.code == t.toets) {
+       t.aantal += 1;
+     }
+   }
+
+   tellers = tellers;
 }
 </script>
 
@@ -25,8 +46,8 @@ function keypress(e) {
 
     <div class="flex flex-row gap-6">
     {#each tellers as teller, i}
-      <div class="flex flex-col p-4 rounded-md shadow-sm bg-violet-300 items-center">
-        <button on:click={() => meer_graag(i)} class="px-6 py-2 mb-4 bg-violet-500 text-white text-xl font-bold rounded-md
+      <div class="flex flex-col p-4 rounded-md shadow-sm bg-violet-300 items-center hover:scale-105 hover:shadow-md transition-all">
+        <button on:click={() => incr(i)} class="px-6 py-2 mb-4 bg-violet-500 text-white text-xl font-bold rounded-md
                 hover:shadow-lg hover:bg-violet-600 active:bg-violet-900 transition-all">{teller.woord}</button>
         <h2 class="text-2xl text-stone-800">{teller.aantal}</h2>
       </div>
